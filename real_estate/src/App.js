d0a4af21e3ca617cb65b2937bb1570e2e1e9737e
PPnaruto <prtaik.35.patil@gmail.com>
2023-02-18 17:30:18 +0530

    Created Basic structure of react app

---
+++ real_estate/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Request from './components/request';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Request/>
+    </div>
+  );
+}
+
+export default App;
---
+++ real_estate/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>ProperyRental.com</h1>
+        <div>
+            <input type="text" />
+            <button>Search</button>
+        </div>
+        <h2>User DashBoard</h2>
+        <button>Login/Sign-Up</button>     
+    </div>
+  )
+}
+
+export default Navbar
